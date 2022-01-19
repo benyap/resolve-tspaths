2a2b64aefd157dde271fb4a51d7cf912c6caeac5
qiaoruntao <925783095@qq.com>
2022-01-19 10:52:59 +0800

    fix: normalize path for fast-glob as fast-glob>3.0 doesn't support windows path; use absolute path in getFilesToProcess test

--- src/steps/getFilesToProcess.ts
+++ src/steps/getFilesToProcess.ts
@@ -1,5 +1,9 @@
-import { resolve } from "path";
-import { sync } from "fast-glob";
+import {resolve} from "path";
+import {sync} from "fast-glob";
+
+function convertPath(windowsPath: string) {
+    return windowsPath.replace(/^\\\\\?\\/, "").replace(/\\/g, '\/').replace(/\/\/+/g, '\/');
+}
 
 /**
  * Get the files in the output directory that should be processed.
@@ -8,14 +12,15 @@ import { sync } from "fast-glob";
  * @param extensions A comma separated list of extensions to match.
  */
 export function getFilesToProcess(outPath: string, extensions: string) {
-  const extensionsList = extensions.split(",");
+    const normalizedOutPath = convertPath(outPath);
+    const extensionsList = extensions.split(",");
 
-  let glob = "*";
-  if (extensionsList.length === 1) glob = `*.${extensionsList[0]}`;
-  else if (extensionsList.length > 1) glob = `*.{${extensionsList.join(",")}}`;
+    let glob = "*";
+    if (extensionsList.length === 1) glob = `*.${extensionsList[0]}`;
+    else if (extensionsList.length > 1) glob = `*.{${extensionsList.join(",")}}`;
 
-  return sync(`${outPath}/**/${glob}`, {
-    dot: true,
-    onlyFiles: true,
-  }).map((path) => resolve(path));
+    return sync(`${normalizedOutPath}/**/${glob}`, {
+        dot: true,
+        onlyFiles: true,
+    }).map((path) => resolve(path));
 }
