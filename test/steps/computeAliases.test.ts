add317647f0cbcbcf1c9faab1a8d966d5f23e85b
Felix Viernickel <f.viernickel@googlemail.com>
2022-03-05 12:51:30 +0100

    test: add test-case for match-all glob

--- test/steps/computeAliases.test.ts
+++ test/steps/computeAliases.test.ts
@@ -5,18 +5,21 @@ describe("steps/computeAliases", () => {
     const aliases = computeAliases(".", {
       compilerOptions: {
         paths: {
+          "*": ["./lib/*"],
           "~/*": ["./src/*", "./root/*"],
           "@app": ["./src/app/*"],
         },
       },
     });
 
-    expect(aliases).toHaveLength(2);
-    expect(aliases[0].prefix).toEqual("~/");
-    expect(aliases[1].prefix).toEqual("@app");
+    expect(aliases).toHaveLength(3);
+    expect(aliases[0].prefix).toEqual("");
+    expect(aliases[1].prefix).toEqual("~/");
+    expect(aliases[2].prefix).toEqual("@app");
 
     const cwd = process.cwd();
-    expect(aliases[0].aliasPaths).toEqual([`${cwd}/src`, `${cwd}/root`]);
-    expect(aliases[1].aliasPaths).toEqual([`${cwd}/src/app`]);
+    expect(aliases[0].aliasPaths).toEqual([`${cwd}/lib`]);
+    expect(aliases[1].aliasPaths).toEqual([`${cwd}/src`, `${cwd}/root`]);
+    expect(aliases[2].aliasPaths).toEqual([`${cwd}/src/app`]);
   });
 });
