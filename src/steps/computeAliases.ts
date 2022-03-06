683d8a1b8bcea27524e4d5fca5a47babcc2bf5f8
Ben Yap <contact@benyap.com>
2022-03-06 18:17:16 +1100

    chore: format

--- src/steps/computeAliases.ts
+++ src/steps/computeAliases.ts
@@ -9,12 +9,11 @@ export function computeAliases(basePath: string, tsConfig: TSConfig): Alias[] {
   const regex = /\*$/;
   const { compilerOptions = {} } = tsConfig;
   const paths = compilerOptions.paths ?? {};
-  const aliases: Alias[] = Object.keys(paths)
-    .map((alias) => ({
-      prefix: alias.replace(regex, ""),
-      aliasPaths: paths[alias].map((path: string) =>
-        resolve(basePath, path.replace(regex, ""))
-      ),
-    }));
+  const aliases: Alias[] = Object.keys(paths).map((alias) => ({
+    prefix: alias.replace(regex, ""),
+    aliasPaths: paths[alias].map((path: string) =>
+      resolve(basePath, path.replace(regex, ""))
+    ),
+  }));
   return aliases;
 }
