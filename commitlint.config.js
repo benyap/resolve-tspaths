fc5a5b3fd7831c165a8acd88a6bc9406589ebaf6
Ben Yap <contact@benyap.com>
2022-05-14 16:45:46 +1000

    chore: add deps to commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "type-enum": [
       2,
       "always",
-      ["feat", "fix", "chore", "wip", "test", "docs", "tooling", "revert"],
+      [
+        "feat",
+        "fix",
+        "chore",
+        "wip",
+        "deps",
+        "test",
+        "docs",
+        "tooling",
+        "revert",
+      ],
     ],
   },
 };
