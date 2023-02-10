48a2376cbc0d380714fca4120d5d620a38db61a1
Ben Yap <contact@benyap.com>
2023-02-10 22:49:44 +1100

    chore: use pnpm

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,7 +4,7 @@ module.exports = {
     "type-enum": [
       2,
       "always",
-      ["feat", "fix", "chore", "wip", "deps", "docs", "tooling", "revert"],
+      ["feat", "fix", "chore", "revert", "docs", "deps", "tooling"],
     ],
   },
 };
